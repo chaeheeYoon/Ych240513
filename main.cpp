279b19a9b6221a965dfb367176be2e6b7614b913
ych <korea940414@naver.com>
2024-05-13 10:13:02 +0900

    사칙연산 삽입.

--- main.cpp
+++ main.cpp
@@ -2,9 +2,20 @@
 
 using namespace std;
 
-int main()
-{
+int main()  // cpu는 정수계산에 특화 / 어지간해서는 int
+{			// 코딩표준 -> 첫글자는 대문자
+	int Number1 = 0; // 초기화를 해줘야한다 0; 넣어 줄 것 아니면 이상한 값이 들어간다.
+	int Number2 = 0; // 책에서는 int Number1, Number2 = 0; 처럼 줄여 나올텐데 이거 하지마라. 무조건 직관성 있게
 
+	cin >> Number1;  // cin cout은 Console In, Consle Out 즉, 입출력 할 것 이다.
+	cin >> Number2;
+	//입금하고 결제하고 송금하고 만명단위 -> 틀려봐 난리나지
+	cout << Number1 + Number2 << endl;
+	cout << Number1 - Number2 << endl;
+	cout << Number1 / Number2 << endl;
+	cout << Number1 * Number2 << endl;
 
 	return 0;
+
+	// 이후부턴 버전관리 해보겠다
 }
